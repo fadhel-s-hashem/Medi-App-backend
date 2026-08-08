92586eadd1b79fecf34620027179991cfa816915
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-08 13:22:15 +0300

    patient model

---
+++ model/patient.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const patientSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+    },
+    CPR:{
+        type:String,
+        require:true,
+    },
+    phroneNumber:{
+        type: String,
+        require: true
+    },
+    gender:{
+        type: String,
+        enum:['male', 'female'],
+        lowercase: true,
+        default: 'male',
+    },
+    birthDate:{
+        type: Date,
+
+    }
+
+} ,{timestamps: true})
+
+const Patient = mongoose.model('Patient' , patientSchema)
+
+module.exports = Patient
